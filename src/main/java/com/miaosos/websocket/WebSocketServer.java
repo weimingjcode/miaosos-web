d903dfea403ff634ded7c017c0bb037ca4f2cd82
weimingjcode <waczjm@126.com>
2020-01-13 02:22:49 +0800

    second ttttttttttt

--- src/main/java/com/miaosos/websocket/WebSocketServer.java
+++ src/main/java/com/miaosos/websocket/WebSocketServer.java
@@ -29,7 +29,7 @@ public class WebSocketServer {
     public void onOpen(Session session) {
         this.session = session;
         webSocketSet.add(this);     //加入set中
-        addOnlineCount();           //在线数加1
+        addOnlineCount();           //在线数加1    dasdasdasdasdasdas
         log.info("有新连接加入！当前在线人数为" + getOnlineCount());
         try {
             sendMessage("连接成功");
